dd0e55731d87731a67ef5443b3a24a394a6ebba0
Hackersploit__xxSploitation <78088921+hackersploitxxsploitation@users.noreply.github.com>
2023-04-03 19:19:45 -0300

    Update FileCrypto.c

--- FileCrypto.c
+++ FileCrypto.c
@@ -1,7 +1,12 @@
 #include "FileCrypto.h"
 void crypto_file(char *name) {
 
+HANDLE file;//Um identifificaador para o arquivo 
+	PBYTE pbPlaintextFileData = NULL;
+	DWORD dwPlaintextFileDataLen = 0;
 
+	PBYTE pbEncryptedFileData = NULL;
+	DWORD dwEncryptedFileDataLen = 0;
 
 
 
